a482b9798e95ac6d7cd14277158bc76997cb6834
gcmarques00 <goncamarqs@gmail.com>
2026-06-06 13:35:41 +0100

    sarima modelling

--- Modelling.R
+++ Modelling.R
@@ -1,55 +1,59 @@
-## Model SARIMA
-
-library(forecast)install.packages("astsa")
+library(forecast)
 library(astsa)
-install.packages("lmtest")
-library("lmtest")
+library(lmtest)
+library(tseries)
 
-# Create ts object with weekly seasonality
 consumo_ts <- ts(df$CONSUMO, frequency = 7)
-# with yearly seasonality
-#consumo_ts <- ts(df$CONSUMO, start = c(2010, 1), frequency = 365.25)
 
-# Candidate models
-m1 <- Arima(consumo_ts, order = c(2,1,1), seasonal = c(1,1,1))
-m2 <- Arima(consumo_ts, order = c(1,1,1), seasonal = c(1,1,1))
-m3 <- Arima(consumo_ts, order = c(2,1,0), seasonal = c(1,1,1))
-m4 <- Arima(consumo_ts, order = c(0,1,1), seasonal = c(0,1,1))
+ggtsdisplay(consumo_ts, series = "Energia")
 
-# Compare AIC/BIC
+adf_test <- adf.test(consumo_ts)
+print(adf_test)
+
+kpss_result <- kpss.test(consumo_ts)
+print(kpss_result)
+
+m1 <- Arima(consumo_ts, order = c(1,1,0), seasonal = c(1,1,0))
+m2 <- Arima(consumo_ts, order = c(1,1,0), seasonal = c(2,1,0))
+m3 <- Arima(consumo_ts, order = c(2,1,0), seasonal = c(1,1,0))
+m4 <- Arima(consumo_ts, order = c(2,1,0), seasonal = c(2,1,0))
+
 data.frame(
-  Model = c("SARIMA(2,1,1)(1,1,1)[7]",
-            "SARIMA(1,1,1)(1,1,1)[7]",
-            "SARIMA(2,1,0)(1,1,1)[7]",
-            "SARIMA(0,1,1)(0,1,1)[7]"),
+  Model = c("SARIMA(1,1,0)(1,1,0)[7]",
+            "SARIMA(1,1,0)(2,1,0)[7]",
+            "SARIMA(2,1,0)(1,1,0)[7]",
+            "SARIMA(2,1,0)(2,1,0)[7]"),
   AIC = c(AIC(m1), AIC(m2), AIC(m3), AIC(m4)),
   BIC = c(BIC(m1), BIC(m2), BIC(m3), BIC(m4))
 )
 
-# auto.arima suggest a model
-auto_model <- auto.arima(consumo_ts, stepwise = FALSE, approximation = FALSE)
-summary(auto_model)
-AIC(auto_model)
-BIC(auto_model)
+checkresiduals(m4)
 
-summary(m2)
-coeftest(m2)
-checkresiduals(m2)
-Box.test(residuals(m2), lag = 14, type = "Ljung-Box")
-Box.test(residuals(m2), lag = 20, type = "Ljung-Box")
+m5 <- Arima(consumo_ts, order = c(2,1,0), seasonal = c(2,1,1))
+m6 <- Arima(consumo_ts, order = c(2,1,1), seasonal = c(2,1,0))
+m7 <- Arima(consumo_ts, order = c(2,1,1), seasonal = c(2,1,1))
 
-checkresiduals(auto_model)
-Box.test(residuals(auto_model), lag = 14, type = "Ljung-Box")
-Box.test(residuals(auto_model), lag = 20, type = "Ljung-Box")
+data.frame(
+  Model = c("SARIMA(2,1,0)(2,1,1)[7]",
+            "SARIMA(2,1,1)(2,1,0)[7]",
+            "SARIMA(2,1,1)(2,1,1)[7]"),
+  AIC = c(AIC(m5), AIC(m6), AIC(m7)),
+  BIC = c(BIC(m5), BIC(m6), BIC(m7))
+)
 
-sarima(df$CONSUMO, p=1, d=1, q=1, P=1, D=1, Q=1, S=7)
-sarima(df$CONSUMO, p=0, d=0, q=4, P=0, D=1, Q=1, S=7)
+coeftest(m7)
+checkresiduals(m7)
 
-# Matriz de correlação dos parâmetros do modelo final
-library(forecast)
+m8 <- Arima(consumo_ts, order = c(1,1,1), seasonal = c(2,1,1))
+m9 <- Arima(consumo_ts, order = c(1,1,1), seasonal = c(1,1,1))
 
-# Covariância dos parâmetros
-vcov(m2_clean)
+data.frame(
+  Model = c("SARIMA(1,1,1)(2,1,1)[7]", "SARIMA(1,1,1)(1,1,1)[7]"),
+  AIC = c(AIC(m8), AIC(m9)),
+  BIC = c(BIC(m8), BIC(m9))
+)
 
-# Correlação dos parâmetros
-cov2cor(vcov(m2_clean))
+coeftest(m9)
+checkresiduals(m9)
+
+cov2cor(vcov(m9))
